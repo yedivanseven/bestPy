c7ec67700402a4f5334677b3180fbbfae8e05b8e
Yedi van Seven <georg@muckisnspirit.com>
2017-04-14 18:53:07 +0200

    Refactored UserItemMatrix

--- datastructures/useritemmatrix.py
+++ datastructures/useritemmatrix.py
@@ -10,26 +10,26 @@ class UserItemMatrix(Transactions):
 
     @property
     def number_of_users(self):
-        if not hasattr(self, '_UserItemMatrix__number_of_users'):
+        if not self.__has('number_of_users'):
             self.__number_of_users = len(self.userIndex_of)
         return self.__number_of_users
 
     @property
     def number_of_items(self):
-        if not hasattr(self, '_UserItemMatrix__number_of_items'):
+        if not self.__has('number_of_items'):
             self.__number_of_items = len(self.itemIndex_of)
         return self.__number_of_items
 
     @property
     def min_matrix_shape(self):
-        if not hasattr(self, '_UserItemMatrix__min_matrix_shape'):
+        if not self.__has('min_matrix_shape'):
             self.__min_matrix_shape = min(self.number_of_users,
                                           self.number_of_items)
         return self.__min_matrix_shape
 
     @property
     def userID_of(self):
-        if not hasattr(self, '_UserItemMatrix__userID_of'):
+        if not self.__has('userID_of'):
             self.__userID_of = {
                 index: user for user, index in self.userIndex_of.items()
             }
@@ -37,7 +37,7 @@ class UserItemMatrix(Transactions):
 
     @property
     def itemID_of(self):
-        if not hasattr(self, '_UserItemMatrix__itemID_of'):
+        if not self.__has('itemID_of'):
             self.__itemID_of = {
                 index: item for item, index in self.itemIndex_of.items()
             }
@@ -45,7 +45,7 @@ class UserItemMatrix(Transactions):
 
     @property
     def matrix_by_col(self):
-        if not hasattr(self, '_UserItemMatrix__matrix_by_col'):
+        if not self.__has('matrix_by_col'):
             counts, indices = tuple(zip(
                 *((count, user_item) for user_item, count
                                       in self.count_buys_of.items()
@@ -57,29 +57,33 @@ class UserItemMatrix(Transactions):
 
     @property
     def bool_matrix_by_col(self):
-        if not hasattr(self, '_UserItemMatrix__bool_matrix_by_col'):
+        if not self.__has('bool_matrix_by_col'):
             self.__bool_matrix_by_col = self.matrix_by_col.copy()
             self.__bool_matrix_by_col.data[:] = 1.0
         return self.__bool_matrix_by_col
 
     @property
     def matrix_by_row(self):
-        if not hasattr(self, '_UserItemMatrix__matrix_by_row'):
+        if not self.__has('matrix_by_row'):
             self.__matrix_by_row = self.matrix_by_col.tocsr()
         return self.__matrix_by_row
 
     @property
     def bool_matrix_by_row(self):
-        if not hasattr(self, '_UserItemMatrix__bool_matrix_by_row'):
+        if not self.__has('bool_matrix_by_row'):
             self.__bool_matrix_by_row = self.matrix_by_row.copy()
             self.__bool_matrix_by_row.data[:] = 1.0
         return self.__bool_matrix_by_row
 
     @property
     def baseline(self):
-        if not hasattr(self, '_UserItemMatrix__baseline'):
+        if not self.__has('baseline'):
             self.__baseline = self.matrix_by_col.getnnz(0)
         return self.__baseline
 
     def users_who_bought(self, items):
         return unique(self.matrix_by_col[:, items].indices)
+
+    def __has(self, attribute):
+        class_prefix = '_' + self.__class__.__name__ + '__'
+        return hasattr(self, class_prefix + attribute)
